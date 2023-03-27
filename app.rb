bf7bbc4e3c07b75269b149828c6a325f843fa0f2
umut <umut_baykan@hotmail.com>
2023-03-27 12:02:37 +0100

    initial project setup

---
+++ app.rb
@@ -0,0 +1,12 @@
+# file: app.rb
+
+require_relative 'lib/database_connection'
+
+DatabaseConnection.connect('music_library')
+
+sql = 'SELECT id, title FROM albums;'
+result = DatabaseConnection.exec_params(sql, [])
+
+result.each do |record|
+  p record
+end
